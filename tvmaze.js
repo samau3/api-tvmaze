d5ddd9e3cc830b76191a95851cca4acb14380543
Colin Sidberry <colinsidberry@colins-mbp.lan>
2021-09-01 16:28:27 -0700

    enhanced doc strings and cleaned up function logic based on code review feedback

--- tvmaze.js
+++ tvmaze.js
@@ -4,15 +4,15 @@ const $showsList = $("#showsList");
 const $episodesArea = $("#episodesArea");
 const $searchForm = $("#searchForm");
 
+const DEFAULT_IMAGE_URL = "https://tinyurl.com/tv-missing";
 const BASE_SEARCH_URL = "http://api.tvmaze.com/search/shows";
 
 /**
- * Access TVMaze API with query term and a base URL
- * Returns promise of search result information in a JS object
+ * 
+ * Given query term and api URL, return promise of search results from api get request.
  */
-//Given query term and api URL, return results of api get request at the 
-//Note: stronger doc string
-async function getTVMazeAPI(queryTerm, baseURL) { 
+
+async function getSearchAPIResults(queryTerm, baseURL) { 
   let apiResult = await axios.get(baseURL, { params: { q: queryTerm } });
   return apiResult;
 }
@@ -32,14 +32,53 @@ async function getShowsByTerm(queryTerm) {//Note: See line 71 and pull out requi
   // pass in search term into API get request
   // should return a JSON of results for search term
 
-  let shows = await getTVMazeAPI(queryTerm, BASE_SEARCH_URL);//Note: this contains more than shows (e.g. results)
-  shows = shows.data;
-  // console.log("shows.data", shows.data)
-  // console.log("shows.data[0]", shows.data[0])
-  console.log(shows)
-  console.log(shows[0])
-  return shows;
+  let results = await getSearchAPIResults(queryTerm, BASE_SEARCH_URL);//Done: this contains more than shows (e.g. results)
+  let shows = results.data;
+  // console.log("shows", shows);
+  // console.log("shows[0]", shows[0]);
+  // console.log(shows)
+  // console.log(shows[0])
+
+  //loop over shows array
+  //in each loop access show and set the variables for {id, name, summary, image}
+  //return array of objects stripped down to the four items needed
+  // let showsArray = []; //note: rename
+  // for (let i = 0; i < shows.length; i++){
+  //   // let id, name, summary
+  //   // console.log(`shows[${i}]: `, shows[i].show);
+
+  //   //initial variable assignment
+  //   let {id, name, summary, image} = shows[i].show;
+  //   image = image.medium;
+
+  //   //check for null
+  //   id = checkForNullInfo(id); //Note: this shouldn't be in the populating DOM section
+  //   image = checkForNullImage(image);
+  //   name = checkForNullInfo(name);
+  //   summary = checkForNullInfo(summary);
+
+  //   //populate array to be returned accordingly
+  //   showsArray.push({id, name, summary, image});
+  // }
+  // console.log("showsArray: ", showsArray);
+
+  let showsArray = shows.map((show)=> aPIManipulation(show)); //note: change name
+  return showsArray;
 }
+
+function aPIManipulation(show) { //note: maybe change name
+  let {id, name, summary, image} = show.show;
+
+  //check for null
+  id = checkForNullInfo(id); //Note: this shouldn't be in the populating DOM section
+  image = checkForNullImage(image);
+  name = checkForNullInfo(name);
+  summary = checkForNullInfo(summary);
+  
+
+  return {id, name, summary, image};
+}
+
 
 /**
  * Check if object key value exists
@@ -47,12 +86,16 @@ async function getShowsByTerm(queryTerm) {//Note: See line 71 and pull out requi
  * If not, return the string "No Information"
  */
 
-function checkForNullInfo(showProperty) {//Note: try a ternery instead
-  if (!showProperty) {
-    return "No Information"
-  }
-  return showProperty
+function checkForNullInfo(showProperty) {//Done: try a ternery instead
+  // if (!showProperty) {
+  //   return "No Information"
+  // }
+  // return showProperty
+
+  return(showProperty ? showProperty : "No Information");
 }
+
+
 
 /**
  * Check image exist.
@@ -60,11 +103,12 @@ function checkForNullInfo(showProperty) {//Note: try a ternery instead
  * If not, return placeholder image.
  */
 
-function checkForNullImage(image) { //Note: try a ternery instead
-  if (!image) {
-    return "https://tinyurl.com/tv-missing"; //Note: make this a global constant
-  }
-  return image.medium
+function checkForNullImage(image) { //Done: try a ternery instead
+  // if (!image) {
+  //   return DEFAULT_IMAGE_URL; //Done: make this a global constant
+  // }
+  // return image.medium
+  return image ? image.medium : DEFAULT_IMAGE_URL;
 }
 /** Given list of shows, create markup for each and add to DOM */
 
@@ -73,22 +117,22 @@ function populateShows(shows) {
 
   for (let show of shows) {
 
-    let id = checkForNullInfo(show.show.id); //Note: this shouldn't be in the populating DOM section
-    let image = checkForNullImage(show.show.image);
-    let name = checkForNullInfo(show.show.name);
-    let summary = checkForNullInfo(show.show.summary);
+    // let id = checkForNullInfo(show.show.id); //Note: this shouldn't be in the populating DOM section
+    // let image = checkForNullImage(show.show.image);
+    // let name = checkForNullInfo(show.show.name);
+    // let summary = checkForNullInfo(show.show.summary);
 
 
     const $show = $(
-      `<div data-show-id="${id}" class="Show col-md-12 col-lg-6 mb-4">
+      `<div data-show-id="${show.id}" class="Show col-md-12 col-lg-6 mb-4">
          <div class="media">
            <img 
-              src="${image}" 
-              alt="${name}" 
+              src="${show.image}" 
+              alt="${show.name}" 
               class="w-25 mr-3">
            <div class="media-body">
-             <h5 class="text-primary">${name}</h5>
-             <div><small>${summary}</small></div>
+             <h5 class="text-primary">${show.name}</h5>
+             <div><small>${show.summary}</small></div>
              <button class="btn btn-outline-light btn-sm Show-getEpisodes">
                Episodes
              </button>
